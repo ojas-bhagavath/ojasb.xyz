153abee39d505c47550a034cb4eb477b14e0886f
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2025-02-24 22:09:10 +0530

    Made darkmode to be the default

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,20 +1,20 @@
-let darkmode = localStorage.getItem("darkmode");
+let lightmode = localStorage.getItem("lightmode");
 
 const themeSwitch = document.getElementById("theme-switch");
 
-const enableDarkmode = () => {
-    document.body.classList.add("darkmode");
-    localStorage.setItem("darkmode", "active");
+const enableLightmode = () => {
+    document.body.classList.add("lightmode");
+    localStorage.setItem("lightmode", "active");
 };
 
-const disableDarkmode = () => {
-    document.body.classList.remove("darkmode");
-    localStorage.setItem("darkmode", null);
+const disableLightmode = () => {
+    document.body.classList.remove("lightmode");
+    localStorage.setItem("lightmode", null);
 };
 
-if (darkmode === "active") enableDarkmode();
+if (lightmode === "active") enableLightmode();
 
 themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem("darkmode");
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+    lightmode = localStorage.getItem("lightmode");
+    lightmode !== "active" ? enableLightmode() : disableLightmode();
 });
